58f7dcbc86ef9291b9ac563eb7ea93ef41c5e480
Evdokimanna <evdokimova.aal@students.dvfu.ru>
2021-11-12 20:42:06 +0300

    git config --global user.email evdokimova.aal@students.dvfu.ru git config --global user.name Evdokimanna

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 using namespace std;
 int main() {
+    setlocale(LC_ALL,"ru");
     int a,b,c;
+    cout<<"Введите переменную1="<<"Введите переменную2"<<"Введите переменную"<<endl;
     cin>>a>>b>>c;
-    cout<<"Formula="<<a+b+c<<endl;
+    cout<<a<<b<<c;
     return 0;
 }
